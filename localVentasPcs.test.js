efd3ddaa49ec2aebc45e568d671cf8fe6b853e23
Mary <31746717+maryangeldavila@users.noreply.github.com>
2019-12-01 14:58:16 -0300

    ventasSucursal

--- localVentasPcs.test.js
+++ localVentasPcs.test.js
@@ -1,6 +1,11 @@
 const ventasVendedora = require("./localVentasPcs").ventasVendedora;
+const ventasSucursal = require("./localVentasPcs").ventasSucursal;
 
 test("Recibe vendedora y retorna importe total de ventas", () => {
   let ventasDe = ventasVendedora("Grace");
   expect(ventasDe).toBe(990);
+});
+
+  let ventasDe = ventasSucursal("Centro");
+  expect(ventasDe).toBe(960);
 });
