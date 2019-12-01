750bbd3607900a23c0322a31291ecd2618a63282
Mary Angel Davila <mary.angel.davila@gmail.com>
2019-12-01 17:55:28 -0300

    Test unificados

--- localVentasPcs.js
+++ localVentasPcs.js
@@ -85,7 +85,6 @@ const precioMaquina = componentes => {
 const cantidadVentasComponentes = componente => {
   let cantidadVentas = 0;
   for (let i = 0; i < ventas.length; i++) {
-    console.log(ventas[i]);
     for (let j = 0; j < ventas[i][6].length; j++) {
       if (componente === ventas[i][6][j]) {
         cantidadVentas++;
